af250590d9aaf582967927483c6d0428d740f843
Gowtham <118418246+Gowthamsubramani12@users.noreply.github.com>
2024-04-10 13:56:10 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "gowtham001" # Replace with your actual S3 bucket name
+    bucket = "gowtham0001" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
